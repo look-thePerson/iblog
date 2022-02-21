d987722d47b01deaa5b2371eca7943f9c18d0195
晓东 <qinxiaodong0511@126.com>
2022-02-21 16:24:05 +0800

    Category加上verbose_name

--- iblog/blog/models.py
+++ iblog/blog/models.py
@@ -16,6 +16,9 @@ class Category(models.Model):
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
+    class Meta:
+        verbose_name = verbose_name_plural = '分类'
+
 
 class Tag(models.Model):
     STATUS_NORMAL = 1
